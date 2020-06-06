a88e66970ce49941ea71344fb0bdd80adab8f382
ian18ishar <ishar@alterra.id>
2020-06-07 05:40:27 +0700

    cicd

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM nginx:stable
-MAINTAINER Bayu Aji  "aji@alterra.id"
 
 RUN mkdir -p /alterra/www/reak
 RUN mkdir -p /alterra/logs/nginx
--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 # fill the env file
 #sed -i -e 's|REAK_STAGING_SONAR_URL|'"${REAK_STAGING_SONAR_URL}"'|g' docker-staging.env
 
-sudo docker build -f "Dockerfile" -t ajisepulsa/alta:frontend-$TRAVIS_BUILD_NUMBER  --label "Maintaner  Bayu Aji <aji@alterra.id>" .
+sudo docker build -f "Dockerfile" -t ian18ishar/fe:$TRAVIS_BUILD_NUMBER .
 # push apps image to docker hub
-sudo docker push ajisepulsa/alta:frontend-$TRAVIS_BUILD_NUMBER
+sudo docker push ian18ishar/fe:$TRAVIS_BUILD_NUMBER
 
-# go inside kubernetes Server
-sed -i -e 's|KUBE_STAGING_CA_CERT|'"${KUBE_STAGING_CA_CERT}"'|g' kubeconfig
-sed -i -e 's|KUBE_STAGING_ENDPOINT|'"${KUBE_STAGING_ENDPOINT}"'|g' kubeconfig
+#echo $(GCLOUD_SERVICE_KEY_PRD) | base64 --decode -i > ${HOME}/gcloud-service-key.json
+#gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
-# set config for aws kredential
-sed -i -e 's|AWS_STAGING_ACCESS_KEY|'"${AWS_STAGING_ACCESS_KEY}"'|g' ~/.aws/credentials
-sed -i -e 's|AWS_STAGING_SECRET_KEY|'"${AWS_STAGING_SECRET_KEY}"'|g' ~/.aws/credentials
+#gcloud container clusters get-credentials $(CLUSTER_NAME_PRD) --zone ${CLOUDSDK_COMPUTE_ZONE} --project $(PROJECT_NAME_PRD)
 
-# update apps deployment
-kubectl -n coba --kubeconfig kubeconfig set image deployment/${APP_K8S_DEPLOYMENT_NAME} ${APP_K8S_CONTAINER_NAME}=ajisepulsa/alta:frontend-${TRAVIS_BUILD_NUMBER}
+#kubectl config view
+#kubectl config current-context
+
+ ## deploy api server
+#kubectl -n ian set image deployment/app magellan-api=$(TAG_PRODUCTION)
