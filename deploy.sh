52baa280b8f43952dfb68eee9d2c99674a57b3b1
ian18ishar <ishar@alterra.id>
2020-06-07 13:22:40 +0700

    cicd3

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ sudo docker push ian18ishar/fe:$TRAVIS_BUILD_NUMBER
 echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
-gcloud container clusters get-credentials $CLUSTER_NAME_PRD --zone $CLOUDSDK_COMPUTE_ZONE --project $PROJECT_NAME_PRD
+gcloud container clusters get-credentials $CLUSTER_NAME_PRD --region $CLOUDSDK_COMPUTE_ZONE --project $PROJECT_NAME_PRD
 
 kubectl config view
 kubectl config current-context
