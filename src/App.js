4162f9f7d3cf373fbea745e41407aa4691ea58a6
razin <razin@alterra.id>
2020-06-07 16:17:10 +0700

    FE Facelift

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          File <code>src/App.js</code> Sudah di edit oleh INFRA.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
