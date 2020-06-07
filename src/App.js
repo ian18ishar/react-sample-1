3be515a1c9a70fd44a5a704a2e52eb92b8a0989c
razin <razin@alterra.id>
2020-06-07 17:36:16 +0700

    Build on AIR

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pulsa anda habis ? Beli disini AJA!
+          Pulsa anda habis ? Beli disini AJA, PALING MURAH SE-Indonesia Raya
         </p>
         <a
           className="App-link"
