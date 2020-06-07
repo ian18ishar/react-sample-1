6ceeaf9ec1a2d3aaf02e7cfe635ea21ded871f73
ian18ishar <ishar@alterra.id>
2020-06-07 12:24:43 +0700

    cicd1

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 sudo docker build -f "Dockerfile" -t ian18ishar/fe:$TRAVIS_BUILD_NUMBER .
 sudo docker push ian18ishar/fe:$TRAVIS_BUILD_NUMBER
 
-#echo $(GCLOUD_SERVICE_KEY_PRD) | base64 --decode -i > ${HOME}/gcloud-service-key.json
-#gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+echo $(GCLOUD_SERVICE_KEY_PRD) | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
-#gcloud container clusters get-credentials $(CLUSTER_NAME_PRD) --zone ${CLOUDSDK_COMPUTE_ZONE} --project $(PROJECT_NAME_PRD)
+gcloud container clusters get-credentials $(CLUSTER_NAME_PRD) --zone ${CLOUDSDK_COMPUTE_ZONE} --project $(PROJECT_NAME_PRD)
 
-#kubectl config view
-#kubectl config current-context
+kubectl config view
+kubectl config current-context
 
  ## deploy api server
-#kubectl -n ian set image deployment/app magellan-api=$(TAG_PRODUCTION)
+kubectl -n ian set image deployment/app-2048 app-2048=ian18ishar/fe:$TRAVIS_BUILD_NUMBER
